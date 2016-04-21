93ba172ca5286e301c3679d8d658452e42ba8f99
C-M-K <c.kaumbulu@gmail.com>
2016-04-21 09:13:49 +0300

    Update 2.2.cpp

--- 2.2.cpp
+++ 2.2.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
 int main(){
+  
 for (int n =0; n<5; n++)
 {
 cout<<"Hello, World!\n";
 }
+
+int a=0;
 while (a<8){
 cout<<"Hello, World!\n";
-a++
+a++;
 }
+
+int b = 2; int b = 2; 
 do {
-int b = 2; 
 cout<<"Hello, World!\n";
 b++;
 }while (b<20);
