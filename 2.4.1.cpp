3ca228ad731107526c24d564fbabd4448e6b0eef
C-M-K <c.kaumbulu@gmail.com>
2016-04-21 09:33:24 +0300

    Update 2.4.1.cpp

--- 2.4.1.cpp
+++ 2.4.1.cpp
@@ -4,11 +4,13 @@
 using namespace std; 
 int main(){
 int n;  
-do {
+
 cout<<"Enter a number";
 cin>>n;
-cout<<"((0==n%5)&&(n>=0))? n/5:-1"; //where ternary operator goes ... ? sort of if and : sort of else 
-count--; 
-}while(1);
-return 0;
+
+while(1){
+    cout<<(((0==n%5)&&(n>=0))? n/5:-1); //where ternary operator goes ... ? sort of if and : sort of else  
+break; 
+}
+return 0; 
 } 
